6ba0f7dcecff8dae57e4128ce51e599022094cb7
Fernando Guerrero <devrrior@gmail.com>
2024-01-18 18:46:22 -0600

    feat(Project): Setup spring boot project

---
+++ src/main/java/com/example/ecommerceupchv2/EcommerceUpchV2Application.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceupchv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceUpchV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceUpchV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecommerceupchv2/EcommerceUpchV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceupchv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceUpchV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
